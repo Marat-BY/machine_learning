d5aca81c49f652f242265f25b813293ebb06e4ea
Marat <43601384+Marat-BY@users.noreply.github.com>
2019-11-11 10:53:27 +0300

    Update pipeline_for_classification.py

--- KNN/pipeline_for_classification.py
+++ KNN/pipeline_for_classification.py
@@ -36,3 +36,37 @@ y_pred = cv.predict(X_test)
 print('Accuracy: {}'.format(cv.scrore(X_test, y_test)))
 print(classification.report(y_test, y_pred))
 print('Tunned Model Parameters: {}'.format(cv.best_params_))
+
+
+# ========================================================================================
+# Another pipeline for regression problem
+# steps for the pipeline
+# Setup the pipeline steps: steps
+steps = [('imputation', Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)),
+         ('scaler', StandardScaler()),
+         ('elasticnet', ElasticNet())]
+
+# Creating pipeline
+pipeline = Pipeline(steps)
+
+# Hyperparameters space
+parameters = {'elasticnet__11_ratio' : np.linspace(0,1,30)}
+
+# Create a train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+
+# GrodSearchCV:
+gm_cv = GridSearchCV(pipeline, param_grid = parameters)
+
+
+# Fit to the training set and compute the metrics
+gm_cv.fit(X_train, y_train)
+
+# Compute and print the metrics
+r2 = gm_cv.score(X_test, y_test)
+print("Tuned ElasticNet Alpha: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
+
+
+
+# You are welcome
